aa069eb8478a2ba808570438b1ed017da9c911ab
Lucas Machado Zainote <95252125+lucasmz1@users.noreply.github.com>
2026-01-19 16:47:16 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,13 +4,7 @@ set -eu
 
 ARCH=$(uname -m)
 
-echo "Installing package dependencies..."
-echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	nomacs
-
-
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package nomacs
 
 # If the application needs to be manually built that has to be done down here
